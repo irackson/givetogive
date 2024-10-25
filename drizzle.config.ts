9b545ddf504cc2a2dde46facc6be7497f816a538
irackson <inasusr@gmail.com>
2024-10-25 17:19:39 -0400

    useTabs, singleQuote

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,12 @@
-import { type Config } from "drizzle-kit";
+import { type Config } from 'drizzle-kit';
 
-import { env } from "code/env";
+import { env } from 'code/env';
 
 export default {
-  schema: "./src/server/db/schema.ts",
-  dialect: "postgresql",
-  dbCredentials: {
-    url: env.DATABASE_URL,
-  },
-  tablesFilter: ["givetogive_*"],
+	schema: './src/server/db/schema.ts',
+	dialect: 'postgresql',
+	dbCredentials: {
+		url: env.DATABASE_URL,
+	},
+	tablesFilter: ['givetogive_*'],
 } satisfies Config;
--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,9 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 const config = {
-  plugins: ["prettier-plugin-tailwindcss"],
+	plugins: ['prettier-plugin-tailwindcss'],
+	tabWidth: 4,
+	useTabs: true,
+	singleQuote: true,
 };
 
 export default config;
