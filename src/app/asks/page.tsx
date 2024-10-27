501ba184152c3ef77de347a8c98c7678e8b813f2
irackson <inasusr@gmail.com>
2024-10-26 22:53:22 -0400

    update import for renamed file

--- src/app/asks/page.tsx
+++ src/app/asks/page.tsx
@@ -1,18 +1,16 @@
 import { Container, Typography } from '@mui/material';
 import { getServerAuthSession } from 'code/server/auth';
-import { RenderAsksIndex } from './_components/ask';
+import { RenderAsksIndex } from './_components/RenderAsksIndex';
 
 export default async function AsksIndexPage() {
-	// Fetch session to get the logged-in user's ID
 	const session = await getServerAuthSession();
-	const userId = session?.user?.id; // `userId` is defined if the user is logged in
+	const userId = session?.user?.id;
 
 	return (
 		<Container sx={{ py: 4 }}>
 			<Typography variant="h4" component="h1" align="center" gutterBottom>
 				Open Requests
 			</Typography>
-			{/* Pass `userId` as `createdByUserId` if the user is logged in */}
 			<RenderAsksIndex createdByUserId={userId} />
 		</Container>
 	);
