1e8cd00973d6b8b1d3b05d7d639d21e988030fd7
irackson <inasusr@gmail.com>
2024-10-30 22:02:16 -0400

    wrap href

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -48,7 +48,7 @@ export default function RootLayout({
 								</Button>
 								<Button
 									component={Link}
-									href="/asks"
+									href={'/asks'}
 									color="inherit"
 								>
 									Asks
